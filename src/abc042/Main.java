56fff42f76952ad7eae921fdce97ffce5f8c50e3
schwarzahl <schwarzahl131072@gmail.com>
2016-07-23 21:47:37 +0900

    D問題初提出

--- src/abc042/Main.java
+++ src/abc042/Main.java
@@ -2,9 +2,11 @@ package abc042;
 
 import java.util.Scanner;
 public class Main {
+	static final long MOD = 1000000007L;
+
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveA();
+		main.solveD();
 	}
 
 	private void solveA() {
@@ -27,5 +29,50 @@ public class Main {
 		} else {
 			System.out.println("NO");
 		}
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		long H = sc.nextLong();
+		long W = sc.nextLong();
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+
+		long startRowIndex = H - A;
+		long startColIndex = B + 1;
+		long answer = 0L;
+		for (long shiftIndex = 0; startRowIndex - shiftIndex > 0 && startColIndex + shiftIndex <= W; shiftIndex++) {
+			answer += pointComb(H, W, H - A - shiftIndex, B + 1 + shiftIndex);
+			answer %= MOD;
+		}
+		System.out.println(answer);
+	}
+
+	/**
+	 * h行w列の格子について点(r, c)を通る組み合わせを求めます
+	 * @param h 格子の行数
+	 * @param w 格子の列数
+	 * @param r 通る点の行index
+	 * @param c 通る点の列index
+	 * @return C(r + c - 2, c - 1) * C(h - r + w - c, w - c)
+	 */
+	private long pointComb(long h, long w, long r, long c) {
+		return combination(r + c - 2, c - 1) * combination(h - r + w - c, w - c);
+	}
+
+	/**
+	 * n個からm個選ぶ組み合わせの数を求めます
+	 * @param n 全体の数
+	 * @param m 選ぶ数
+	 * @return C(n, m)
+	 */
+	private long combination(long n, long m) {
+		if (n / 2 < m) {
+			return combination(n , n - m);
+		}
+		if (m <= 0) {
+			return 1;
+		}
+		return (combination(n, m - 1) * (n - m + 1) / m) % MOD;
 	}
 }
