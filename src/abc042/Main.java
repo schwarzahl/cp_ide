8ce3732a2afbc08525ad33d50be731179b0a429a
schwarzahl <schwarzahl131072@gmail.com>
2016-07-23 21:05:13 +0900

    A問題初提出

--- src/abc042/Main.java
+++ src/abc042/Main.java
@@ -9,14 +9,23 @@ public class Main {
 
 	private void solveA() {
 		Scanner sc = new Scanner(System.in);
-		// 整数の入力
-		int a = sc.nextInt();
-		// スペース区切りの整数の入力
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		// 文字列の入力
-		String s = sc.next();
-		// 出力
-		System.out.println((a+b+c) + " " + s);
+		int five = 0;
+		int seven = 0;
+		for (int i = 0; i < 3; i++) {
+			switch (sc.nextInt()) {
+			case 5:
+				five++;
+				break;
+			case 7:
+				seven++;
+				break;
+			default:
+			}
+		}
+		if (five == 2 && seven == 1) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
 	}
 }
