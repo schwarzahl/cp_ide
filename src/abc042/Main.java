55f351e222359bf56f4327fcbc7192d1790a6d07
schwarzahl <schwarzahl131072@gmail.com>
2016-07-23 21:01:32 +0900

    とりあえず最低限

---
+++ src/abc042/Main.java
@@ -0,0 +1,22 @@
+package abc042;
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args){
+		Main main = new Main();
+		main.solveA();
+	}
+
+	private void solveA() {
+		Scanner sc = new Scanner(System.in);
+		// 整数の入力
+		int a = sc.nextInt();
+		// スペース区切りの整数の入力
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		// 文字列の入力
+		String s = sc.next();
+		// 出力
+		System.out.println((a+b+c) + " " + s);
+	}
+}
