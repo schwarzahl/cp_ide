5d4b70d44d7545f791a38132804bb4e8bc1f4f2b
schwarzahl <schwarzahl131072@gmail.com>
2017-04-15 19:49:26 +0900

    AGC013の準備

---
+++ src/agc013/Main.java
@@ -0,0 +1,15 @@
+package agc013;
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args){
+		Main main = new Main();
+		main.solveA();
+	}
+
+	private void solveA() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+}
