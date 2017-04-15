b5a9bfe5e890cff82e50d59cb575e801d6c43d18
schwarzahl <schwarzahl131072@gmail.com>
2017-04-15 22:19:12 +0900

    問題Cは多分方針が間違っていたけど、バックアップ用に一応コミット

--- src/agc013/Main.java
+++ src/agc013/Main.java
@@ -1,10 +1,58 @@
 package agc013;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveA();
+		main.solveC();
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int L = sc.nextInt();
+		int T = sc.nextInt();
+		int[] array = new int[N];
+		Set<Integer> set = new HashSet<Integer>();
+		int prev_d = 1;
+		for (int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			int d = sc.nextInt();
+			array[i] = x + T * (3 - d * 2);
+			if (prev_d != d) {
+				set.add((i - 1 + N) % N);
+			}
+		}
+		while (!set.isEmpty()) {
+			Set<Integer> nextSet = new HashSet<Integer>();
+			for (int i : set) {
+				if (checkAndSwap(array, i, (i + 1) % N, N, L)) {
+					nextSet.add((i - 1 + N) % N);
+					nextSet.add((i + 1) % N);
+				}
+			}
+			set = nextSet;
+		}
+		for (int x : array) {
+			System.out.println((x + L) % L);
+		}
+	}
+
+	private boolean checkAndSwap(int[] array, int a, int b, int N, int L) {
+		int offset = 0;
+		if (a == N - 1 && b == 0) {
+			offset = L;
+		}
+		if (array[a] - offset > array[b]) {
+			//System.err.println("swap " + a + " and " + b + " a.x = " + array[a] + " b.x = " + array[b]);
+			int swap = array[b];
+			array[b] = array[a] - offset;
+			array[a] = swap + offset;
+			return true;
+		}
+		return false;
 	}
 
 	private void solveA() {
