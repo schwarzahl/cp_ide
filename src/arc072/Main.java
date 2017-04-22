843971364ff41135762f943d98a0a3979bb81842
schwarzahl <schwarzahl131072@gmail.com>
2017-04-23 00:39:37 +0900

    終わってから解説読んでC問題通した intをlongにするのとロジックをシンプルにした 問題の簡単化が大事

--- src/arc072/Main.java
+++ src/arc072/Main.java
@@ -11,58 +11,39 @@ public class Main {
 
 	private void solveC() {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		List<Integer> list = new ArrayList<Integer>();
+		long n = sc.nextInt();
+		long oddPlusSum = 0;
+		long evenPlusSum = 0;
+		long oddPlusAns = 0;
+		long evenPlusAns = 0;
 		for (int i = 0; i < n; i++) {
-			list.add(sc.nextInt());
-		}
-		int start = list.get(0);
-		int origin = innerSolveC(list);
-		list.set(0, 1);
-		int plus = innerSolveC(list) + Math.abs(start - 1);
-		list.set(0, -1);
-		int minus = innerSolveC(list) + Math.abs(start + 1);
-		if (origin < plus && start != 0) {
-			if (origin < minus) {
-				System.out.println(origin);
+			long tmp = sc.nextLong();
+			oddPlusSum += tmp;
+			evenPlusSum += tmp;
+			if (i % 2 == 0) {
+				if (oddPlusSum >= 0) {
+					oddPlusAns += oddPlusSum + 1;
+					oddPlusSum = -1;
+				}
+				if (evenPlusSum <= 0) {
+					evenPlusAns += 1 - evenPlusSum;
+					evenPlusSum = 1;
+				}
 			} else {
-				if (plus < minus) {
-					System.out.println(plus);
-				} else {
-					System.out.println(minus);
+				if (oddPlusSum <= 0) {
+					oddPlusAns += 1 - oddPlusSum;
+					oddPlusSum = 1;
+				}
+				if (evenPlusSum >= 0) {
+					evenPlusAns += evenPlusSum + 1;
+					evenPlusSum = -1;
 				}
 			}
+		}
+		if (evenPlusAns < oddPlusAns) {
+			System.out.println(evenPlusAns);
 		} else {
-			if (plus < minus) {
-				System.out.println(plus);
-			} else {
-				System.out.println(minus);
-			}
+			System.out.println(oddPlusAns);
 		}
-	}
-
-	private int innerSolveC(List<Integer> list) {
-		int sum = 0;
-		int ans = 0;
-		for (int tmp : list) {
-			if (sum == 0) {
-				sum = tmp;
-			} else if (sum < 0) {
-				if (sum + tmp > 0) {
-					sum += tmp;
-				} else {
-					ans += 1 - (sum + tmp);
-					sum = 1;
-				}
-			} else {
-				if (sum + tmp < 0) {
-					sum += tmp;
-				} else {
-					ans += (sum + tmp) + 1;
-					sum = -1;
-				}
-			}
-		}
-		return ans;
 	}
 }
