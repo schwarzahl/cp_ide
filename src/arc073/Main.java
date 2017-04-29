78f196bdf9fd18421da981c83ed58eea158db24f
schwarzahl <schwarzahl131072@gmail.com>
2017-04-29 20:46:57 +0900

    ARC073 first commit

---
+++ src/arc073/Main.java
@@ -0,0 +1,16 @@
+package arc073;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args){
+		Main main = new Main();
+		main.solveC();
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextInt();
+	}
+}
