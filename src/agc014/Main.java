0b07bfdf8ba8035f4924b94952fad54c8d778b6b
schwarzahl <schwarzahl131072@gmail.com>
2017-05-06 22:36:40 +0900

    AGC014 D問題 とりあえずコミット

--- src/agc014/Main.java
+++ src/agc014/Main.java
@@ -1,10 +1,14 @@
 package agc014;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveC();
+		main.solveD();
 	}
 
 	private void solveA() {
@@ -129,5 +133,51 @@ public class Main {
 			}
 		}
 		return -1;
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		boolean[][] graph = new boolean[N + 1][];
+		for (int j = 1; j <= N; j++) {
+			graph[j] = new boolean[N + 1];
+		}
+		for (int i = 0; i < N - 1; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			graph[a][b] = true;
+			graph[b][a] = true;
+		}
+		if (judge(N, graph)) {
+			System.out.println("First");
+		} else {
+			System.out.println("Second");
+		}
+	}
+
+	private boolean judge(int N, boolean[][] graph) {
+		if (N % 2 == 1) {
+			return true;
+		}
+		int[] leafCloseCount = new int[N + 1];
+		for (int a = 1; a <= N; a++) {
+			int count = 0;
+			int lastNode = 0;
+			for (int b = 1; b <= N; b++) {
+				if (graph[a][b]) {
+					count++;
+					lastNode = b;
+				}
+			}
+			if (count == 1) {
+				leafCloseCount[lastNode]++;
+			}
+		}
+		for (int count : leafCloseCount) {
+			if (count > 1) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
