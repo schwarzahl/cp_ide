6e5a9aefdfb8c58a4fddf25e494bb4e5771d1677
schwarzahl <schwarzahl131072@gmail.com>
2017-05-06 22:59:39 +0900

    AGC014 D問題 奇数長のパスが分岐していれば勝ち

--- src/agc014/Main.java
+++ src/agc014/Main.java
@@ -162,20 +162,37 @@ public class Main {
 		if (N % 2 == 1) {
 			return true;
 		}
-		int[] leafAdjCount = new int[N + 1];
 		for (int a = 1; a <= N; a++) {
-			int count = 0;
-			int lastNode = 0;
 			List<Integer> adjList = graph.get(a);
-			if (adjList.size() == 1) {
-				leafAdjCount[adjList.get(0)]++;
+			int oddPathCount = 0;
+			for (int adjNode : adjList) {
+				int length = path(a, adjNode, graph);
+				if (length % 2 == 1) {
+					oddPathCount++;
+				}
 			}
-		}
-		for (int count : leafAdjCount) {
-			if (count > 1) {
+			if (oddPathCount > 1) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	private int path(int origin, int next, Map<Integer, List<Integer>> graph) {
+		List<Integer> targetList = graph.get(next);
+		if (targetList.size() > 2) {
+			return 0;
+		}
+		for (int target : targetList) {
+			if (target != origin) {
+				int result = path(next, target, graph);
+				if (result == 0) {
+					return 0;
+				} else {
+					return result + 1;
+				}
+			}
+		}
+		return 1;
 	}
 }
