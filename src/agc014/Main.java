70c73624eb2fe5acab89275d00d113110353067c
schwarzahl <schwarzahl131072@gmail.com>
2017-05-06 21:30:04 +0900

    AGC014 B問題 AC、当てずっぽ回答が通った ギルティ

--- src/agc014/Main.java
+++ src/agc014/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveA();
+		main.solveB();
 	}
 
 	private void solveA() {
@@ -21,6 +21,7 @@ public class Main {
 			if (a == b && b == c) {
 				System.out.println("-1");
 				return;
+				
 			}
 			n++;
 			long nextA = (b + c) / 2;
@@ -29,6 +30,28 @@ public class Main {
 			a = nextA;
 			b = nextB;
 			c = nextC;
+		}
+	}
+
+	private void solveB() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] count = new int[N];
+		for (int i = 0; i < M; i++) {
+			count[sc.nextInt() - 1]++;
+			count[sc.nextInt() - 1]++;
+		}
+		int oddNum = 0;
+		for (int num : count) {
+			if (num % 2 == 1) {
+				oddNum++;
+			}
+		}
+		if (oddNum > 1) {
+			System.out.println("NO");
+		} else {
+			System.out.println("YES");
 		}
 	}
 }
