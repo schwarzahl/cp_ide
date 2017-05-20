5f81a6aa460cb27861d648ecfe89950604680707
schwarzahl <schwarzahl131072@gmail.com>
2017-05-20 22:04:47 +0900

    ARC074 D問題 WAとTLEが出たのでオーバーフローの可能性を考えて型をBigIntegerに TLEは防げないと思う

--- src/arc074/Main.java
+++ src/arc074/Main.java
@@ -1,5 +1,6 @@
 package arc074;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -70,14 +71,20 @@ public class Main {
 		for (int i = 0; i < 3 * N; i++) {
 			list.add(sc.nextLong());
 		}
-		List<Long> scoreList = new ArrayList<Long>();
+		List<BigInteger> scoreList = new ArrayList<>();
 		for (int splitIndex = 0; splitIndex < N; splitIndex++) {
 			List<Long> pre = list.subList(0, N + splitIndex);
 			List<Long> suf = list.subList(N + splitIndex, 3 * N);
-			long preMaxSum = pre.stream().mapToLong(i -> i).sorted().skip(splitIndex).sum();
-			long sufMinSum = suf.stream().mapToLong(i -> i).sorted().limit(N).sum();
-			scoreList.add(preMaxSum - sufMinSum);
+
+			BigInteger score = BigInteger.ZERO;
+			for (long l : pre.stream().mapToLong(i -> i).sorted().skip(splitIndex).toArray()) {
+				score = score.add(BigInteger.valueOf(l));
+			}
+			for (long l : suf.stream().mapToLong(i -> i).sorted().limit(N).toArray()) {
+				score = score.subtract(BigInteger.valueOf(l));
+			}
+			scoreList.add(score);
 		}
-		System.out.println(scoreList.stream().max(Comparator.naturalOrder()).get());
+		System.out.println(scoreList.stream().max((o1, o2) -> o1.compareTo(o2)).get());
 	}
 }
