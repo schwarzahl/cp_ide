39cc1e16aa74f62181a668dcece1362966d6ac6d
schwarzahl <schwarzahl131072@gmail.com>
2017-05-20 21:45:51 +0900

    ARC074 D問題 とりあえず前半と後半の切り方をN通りやってみる

--- src/arc074/Main.java
+++ src/arc074/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveC();
+		main.solveD();
 	}
 
 	private void solveC() {
@@ -60,5 +60,24 @@ public class Main {
 			return a - c;
 		}
 		return 0;
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		List<Long> list = new ArrayList<Long>();
+		for (int i = 0; i < 3 * N; i++) {
+			list.add(sc.nextLong());
+		}
+		List<Long> scoreList = new ArrayList<Long>();
+		for (int splitIndex = 0; splitIndex < N; splitIndex++) {
+			List<Long> pre = list.subList(0, N + splitIndex);
+			List<Long> suf = list.subList(N + splitIndex, 3 * N);
+			long preMaxSum = pre.stream().mapToLong(i -> i).sorted().skip(splitIndex).sum();
+			long sufMinSum = suf.stream().mapToLong(i -> i).sorted().limit(N).sum();
+			scoreList.add(preMaxSum - sufMinSum);
+		}
+		System.out.println(scoreList.stream().max(Comparator.naturalOrder()).get());
 	}
 }
