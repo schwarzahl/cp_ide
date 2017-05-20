79a4edff10e6e2e112d51d16f6cdf118d8c6a944
schwarzahl <schwarzahl131072@gmail.com>
2017-05-20 20:11:30 +0900

    ARC074 first commit

---
+++ src/arc074/Main.java
@@ -0,0 +1,15 @@
+package arc074;
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args){
+		Main main = new Main();
+		main.solveC();
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n);
+	}
+}
