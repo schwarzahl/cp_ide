16a02920fd7ee5ee0d7291fe6ae2f8e22dfb462f
schwarzahl <schwarzahl131072@gmail.com>
2017-05-20 23:35:14 +0900

    ARC074終了後 D問題、解説が自分のギリギリで立てた方針と完全一致していたので優先度付きキューで実装してみる

--- src/arc074/Main.java
+++ src/arc074/Main.java
@@ -1,9 +1,10 @@
 package arc074;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
@@ -67,33 +68,33 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
-		List<Numb> list = new ArrayList<>();
+		List<Long> list = new ArrayList<>();
 		for (int i = 0; i < 3 * N; i++) {
-			list.add(new Numb(sc.nextLong(), i));
+			list.add(sc.nextLong());
 		}
-		list.sort((o1, o2) -> o1.value > o2.value ? 1 : -1);
-		List<BigInteger> scoreList = new ArrayList<>();
-		for (int splitIndex = 0; splitIndex <= N; splitIndex++) {
-			final int index = splitIndex;
-			BigInteger score = BigInteger.ZERO;
-			for (Object numb : list.stream().filter(i -> i.index < N + index).skip(splitIndex).toArray()) {
-				score = score.add(BigInteger.valueOf(((Numb)numb).value));
-			}
-			for (Object numb : list.stream().filter(i -> i.index >= N + index).limit(N).toArray()) {
-				score = score.subtract(BigInteger.valueOf(((Numb)numb).value));
-			}
-			scoreList.add(score);
+
+		List<Long> maxSumList = getSumList(list, new PriorityQueue<>(Comparator.naturalOrder()), N);
+		Collections.reverse(list);
+		List<Long> minSumList = getSumList(list, new PriorityQueue<>(Comparator.reverseOrder()), N);
+
+		List<Long> scoreList = new ArrayList<>();
+		for (int splitIndex = N; splitIndex <= 2 * N; splitIndex++) {
+			scoreList.add(maxSumList.get(splitIndex - 1) - minSumList.get(3 * N - 1 - splitIndex));
 		}
-		System.out.println(scoreList.stream().max((o1, o2) -> o1.compareTo(o2)).get());
+		System.out.println(scoreList.stream().max(Comparator.naturalOrder()).get());
 	}
 
-	class Numb {
-		public long value;
-		public int index;
-
-		public Numb(long value, int index) {
-			this.value = value;
-			this.index = index;
+	private List<Long> getSumList(List<Long> list, PriorityQueue<Long> queue, int limitSize) {
+		List<Long> sumList = new ArrayList<>();
+		long sum = 0L;
+		for (long value : list) {
+			queue.add(value);
+			sum += value;
+			if (queue.size() > limitSize) {
+				sum -= queue.remove();
+			}
+			sumList.add(sum);
 		}
+		return sumList;
 	}
 }
