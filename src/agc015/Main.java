b13eaf57400d37fa0757df3c4c365a585a57fb25
schwarzahl <schwarzahl131072@gmail.com>
2017-05-27 21:10:00 +0900

    AGC015 B問題

--- src/agc015/Main.java
+++ src/agc015/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveA();
+		main.solveB();
 	}
 
 	private void solveA() {
@@ -19,5 +19,20 @@ public class Main {
 		} else {
 			System.out.println(max - min + 1);
 		}
+	}
+
+	private void solveB() {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.nextLine();
+		int N = S.length();
+		long sum = 0L;
+		for (int i = 0; i < N; i++) {
+			if (S.charAt(i) == 'U') {
+				sum += 2 * i + 1 * (N - 1 - i);
+			} else {
+				sum += 1 * i + 2 * (N - 1 - i);
+			}
+		}
+		System.out.println(sum);
 	}
 }
