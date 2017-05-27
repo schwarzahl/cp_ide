190e6ed9e99450b014845619b7a49277e336c021
schwarzahl <schwarzahl131072@gmail.com>
2017-05-27 20:59:17 +0900

    AGC015 first commit

---
+++ src/agc015/Main.java
@@ -0,0 +1,13 @@
+package agc015;
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args){
+		Main main = new Main();
+		main.solveA();
+	}
+
+	private void solveA() {
+		Scanner sc = new Scanner(System.in);
+	}
+}
