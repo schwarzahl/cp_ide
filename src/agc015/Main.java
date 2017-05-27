6f38c477078e224b34088f0e5522964572e94319
schwarzahl <schwarzahl131072@gmail.com>
2017-05-28 03:05:11 +0900

    AGC015 C問題を解説読んだ後に解く 累積和の扱いが難しい

--- src/agc015/Main.java
+++ src/agc015/Main.java
@@ -44,107 +44,41 @@ public class Main {
 		int M = sc.nextInt();
 		int Q = sc.nextInt();
 		sc.nextLine();
-		boolean[][] map = new boolean[N][];
-		for (int i = 0; i < N; i++) {
+		boolean[][] map = new boolean[N + 1][];
+		map[0] = new boolean[M + 1];
+		for (int r = 1; r <= N; r++) {
+			map[r] = new boolean[M + 1];
 			String line = sc.nextLine();
-			map[i] = new boolean[M];
-			for (int j = 0; j < M; j++) {
-				map[i][j] = line.charAt(j) == '1';
+			for (int c = 1; c <= M; c++) {
+				map[r][c] = line.charAt(c - 1) == '1';
+			}
+		}
+		int[][] sum_v = new int[N + 1][];
+		int[][] sum_e_r = new int[N + 1][];
+		int[][] sum_e_c = new int[N + 1][];
+		sum_v[0] = new int[M + 1];
+		sum_e_r[0] = new int[M + 1];
+		sum_e_c[0] = new int[M + 1];
+		for (int r = 1; r <= N; r++) {
+			sum_v[r] = new int[M + 1];
+			sum_e_r[r] = new int[M + 1];
+			sum_e_c[r] = new int[M + 1];
+			for (int c = 1; c <= M; c++) {
+				sum_v[r][c] = sum_v[r][c - 1] + sum_v[r - 1][c] - sum_v[r - 1][c - 1] + (map[r][c] ? 1 : 0);
+				sum_e_r[r][c] = sum_e_r[r][c - 1] + sum_e_r[r - 1][c] - sum_e_r[r - 1][c - 1] + (map[r][c] && map[r - 1][c] ? 1 : 0);
+				sum_e_c[r][c] = sum_e_c[r][c - 1] + sum_e_c[r - 1][c] - sum_e_c[r - 1][c - 1] + (map[r][c] && map[r][c - 1] ? 1 : 0);
 			}
 		}
 
-		List<List<Range>> rowBlueRangeList = new ArrayList<List<Range>>();
-		List<List<Range>> colBlueRangeList = new ArrayList<List<Range>>();
-		for (int i = 1; i <= N; i++) {
-			boolean isInRange = false;
-			List<Range> list = new ArrayList<Range>();
-			Range nowRange = null;
-			for (int j = 1; j <= M; j++) {
-				if (!isInRange && map[i - 1][j - 1]) {
-					nowRange = new Range();
-					nowRange.start = j;
-					isInRange = true;
-				}
-				if (isInRange && !map[i - 1][j - 1]) {
-					nowRange.end = j - 1;
-					list.add(nowRange);
-					isInRange = false;
-				}
-			}
-			if (isInRange) {
-				nowRange.end = M;
-				list.add(nowRange);
-			}
-			rowBlueRangeList.add(list);
-		}
-		for (int j = 1; j <= M; j++) {
-			boolean isInRange = false;
-			List<Range> list = new ArrayList<Range>();
-			Range nowRange = null;
-			for (int i = 1; i <= N; i++) {
-				if (!isInRange && map[i - 1][j - 1]) {
-					nowRange = new Range();
-					nowRange.start = i;
-					isInRange = true;
-				}
-				if (isInRange && !map[i - 1][j - 1]) {
-					nowRange.end = i - 1;
-					list.add(nowRange);
-					isInRange = false;
-				}
-			}
-			if (isInRange) {
-				nowRange.end = N;
-				list.add(nowRange);
-			}
-			colBlueRangeList.add(list);
-		}
 		for (int k = 0; k < Q; k++) {
 			int x_1 = sc.nextInt();
 			int y_1 = sc.nextInt();
 			int x_2 = sc.nextInt();
 			int y_2 = sc.nextInt();
-			long sum = 0L;
-			for (int r = x_1; r <= x_2; r++) {
-				sum += countRowBlues(r, y_1, y_2, rowBlueRangeList);
-			}
-			for (int c = y_1; c <= y_2; c++) {
-				sum -= countColBlues(c, x_1, x_2, colBlueRangeList);
-			}
-			System.out.println(sum);
+			int v = sum_v[x_2][y_2] - sum_v[x_1 - 1][y_2] - sum_v[x_2][y_1 - 1] + sum_v[x_1 - 1][y_1 - 1];
+			int r = sum_e_r[x_2][y_2] - sum_e_r[x_1][y_2] - sum_e_r[x_2][y_1 - 1] + sum_e_r[x_1][y_1 - 1];
+			int c = sum_e_c[x_2][y_2] - sum_e_c[x_1 - 1][y_2] - sum_e_c[x_2][y_1] + sum_e_c[x_1 - 1][y_1];
+			System.out.println(v - r - c);
 		}
-	}
-
-	class Range {
-		public int start;
-		public int end;
-	}
-
-	/**
-	 * @return r行目のc1列～c2列の間の青い区間の数
-	 */
-	int countRowBlues(int r, int c1, int c2, List<List<Range>> list) {
-		int count = 0;
-		for (Range range : list.get(r - 1)) {
-			if ((c1 <= range.start && range.start <= c2) || (c1 <= range.end && range.end <= c2)) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	/**
-	 * @return c行目のr1列～r2列の間の青い区間の長さ - 1の和
-	 */
-	int countColBlues(int c, int r1, int r2, List<List<Range>> list) {
-		int length = 0;
-		for (Range range : list.get(c - 1)) {
-			if ((r1 <= range.start && range.start <= r2) || (r1 <= range.end && range.end <= r2)) {
-				int max_start = r1 <= range.start ? range.start : r1;
-				int min_end = r2 <= range.end ? r2 : range.end;
-				length += min_end - max_start;
-			}
-		}
-		return length;
 	}
 }
