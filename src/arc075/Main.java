06dc99110ee69c1e09a990d61d061bee37bc737c
schwarzahl <schwarzahl131072@gmail.com>
2017-06-03 20:49:06 +0900

    ARC075 first commit

---
+++ src/arc075/Main.java
@@ -0,0 +1,13 @@
+package arc075;
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args){
+		Main main = new Main();
+		main.solveC();
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+	}
+}
