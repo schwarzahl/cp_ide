70b7b64eef9fbd8e4d60a398840caf7b22d7d14e
schwarzahl <schwarzahl131072@gmail.com>
2017-06-03 21:06:27 +0900

    ARC075 C問題

--- src/arc075/Main.java
+++ src/arc075/Main.java
@@ -1,5 +1,8 @@
 package arc075;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
@@ -9,5 +12,25 @@ public class Main {
 
 	private void solveC() {
 		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int sum = 0;
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < N; i++) {
+			int val = sc.nextInt();
+			sum += val;
+			list.add(val);
+		}
+		if (sum % 10 != 0) {
+			System.out.println(sum);
+		} else {
+			Collections.sort(list);
+			for (int val : list) {
+				if (val % 10 != 0) {
+					System.out.println(sum - val);
+					return;
+				}
+			}
+			System.out.println(0);
+		}
 	}
 }
