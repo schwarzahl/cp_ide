1a1f5de765f6bd867e89a7a1f57e8c9f5150f659
schwarzahl <schwarzahl131072@gmail.com>
2017-06-03 21:31:41 +0900

    ARC075 D問題

--- src/arc075/Main.java
+++ src/arc075/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveC();
+		main.solveD();
 	}
 
 	private void solveC() {
@@ -32,5 +32,40 @@ public class Main {
 			}
 			System.out.println(0);
 		}
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		List<Integer> list = new ArrayList<Integer>();
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			int val = sc.nextInt();
+			list.add(val);
+			if (max < val) {
+				max = val;
+			}
+		}
+		long minNum = (max + A - 1) / A;
+		long maxNum = (max + B - 1) / B;
+		do {
+			long count = (minNum + maxNum) / 2;
+			if (isWin(count, A, B, list)) {
+				maxNum = count;
+			} else {
+				minNum = count;
+			}
+		} while (minNum + 1 < maxNum);
+		System.out.println(maxNum);
+	}
+
+	private boolean isWin(long count, long A, long B, List<Integer> hps) {
+		long sum = 0;
+		for (int hp : hps) {
+			sum += (hp - count * B + A - B - 1) / (A - B);
+		}
+		return sum <= count;
 	}
 }
