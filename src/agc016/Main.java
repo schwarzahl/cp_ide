697a77d926e393ba06ef5bc0d9483806492afe8c
schwarzahl <schwarzahl131072@gmail.com>
2017-07-01 16:21:16 +0900

    AGC016 C問題WA h*wの格子に-h*wを入れていけばいけるかと思ったらダメだった

--- src/agc016/Main.java
+++ src/agc016/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveB();
+		main.solveC();
 	}
 
 	private void solveA() {
@@ -81,5 +81,35 @@ public class Main {
 			return false;
 		}
 		return (minKnum < maxK) && (maxK <= minKnum + maxKnum / 2);
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+
+		int H = sc.nextInt();
+		int W = sc.nextInt();
+		int h = sc.nextInt();
+		int w = sc.nextInt();
+
+		int h_num = (H / h);
+		int w_num = (W / w);
+		if (h_num * w_num * h * w < H * W - (h_num * w_num)) {
+			System.out.println("Yes");
+			for (int i_h = 1; i_h <= H; i_h++) {
+				for (int i_w = 1; i_w <= W; i_w++) {
+					if (i_h % h == 0 && i_w % w == 0) {
+						System.out.print(-(h * w));
+					} else {
+						System.out.print(1);
+					}
+					if (i_w < W) {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+			}
+		} else {
+			System.out.println("No");
+		}
 	}
 }
