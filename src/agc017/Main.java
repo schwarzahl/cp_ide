01cd2fcc1f086a601a8c9c2199d4692fc859f288
schwarzahl <schwarzahl131072@gmail.com>
2017-07-09 21:58:01 +0900

    AGC017 A問題AC combでオーバーフローしていたっぽい

--- src/agc017/Main.java
+++ src/agc017/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		main.solveA();
 	}
 
+	static Long[][] pre;
 	private void solveA() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
@@ -22,13 +23,14 @@ public class Main {
 				oneNum++;
 			}
 		}
-		if (P == 0) {
-			ans++;
+		pre = new Long[101][];
+		for (int i = 0; i < 101; i++) {
+			pre[i] = new Long[101];
 		}
-		for (int i = 1; i <= N; i++) {
-			for (int z = 0; z <= zeroNum && z <= i; z++) {
+		for (int i = 0; i <= N; i++) {
+			for (int z = 0; z <= i; z++) {
 				int o = i - z;
-				if (o <= oneNum && o % 2 == P) {
+				if (o % 2 == P) {
 					ans += comb(zeroNum, z) * comb(oneNum, o);
 				}
 			}
@@ -36,24 +38,20 @@ public class Main {
 		System.out.println(ans);
 	}
 
-	private long comb(long A, long B) {
-		if (A < B) {
-			return 0L;
+	private long comb(int A, int B) {
+		if (pre[A][B] == null) {
+			if (A < B) {
+				return 0L;
+			}
+			if (B == 0) {
+				return 1L;
+			}
+			if (A / 2 < B) {
+				return comb(A, A - B);
+			}
+			pre[A][B] = comb(A - 1, B) + comb(A - 1, B - 1);
 		}
-		if (B == 0) {
-			return 1L;
-		}
-		if (A / 2 < B) {
-			return comb(A, A - B);
-		}
-		long ret = 1L;
-		for (long i = A; i > A - B; i--) {
-			ret *= i;
-		}
-		for (long i = B; i > 0; i--) {
-			ret /= i;
-		}
-		return ret;
+		return pre[A][B];
 	}
 
 	private void solveB() {
