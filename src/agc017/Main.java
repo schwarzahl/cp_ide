48e6a5403344d57c8f0dea30fcfcfe012e65862a
schwarzahl <schwarzahl131072@gmail.com>
2017-07-09 23:04:33 +0900

    AGC017 C問題の部分点AC

--- src/agc017/Main.java
+++ src/agc017/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveA();
+		main.solveC();
 	}
 
 	static Long[][] pre;
@@ -78,5 +78,42 @@ public class Main {
 			}
 		}
 		return false;
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] A = new int[N];
+		int[] sum = new int[N + 1];
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+			sum[A[i]]++;
+		}
+		int[] X = new int[M];
+		int[] Y = new int[M];
+		for (int i = 0; i < M; i++) {
+			X[i] = sc.nextInt();
+			Y[i] = sc.nextInt();
+		}
+		for (int i = 0; i < M; i++) {
+			sum[A[X[i] - 1]]--;
+			A[X[i] - 1] = Y[i];
+			sum[Y[i]]++;
+			int current = 0;
+			int num = 0;
+			for (int j = N; j > 0; j--) {
+				if (current < sum[j]) {
+					current = sum[j];
+				}
+				if (current > 0) {
+					current--;
+				} else {
+					num++;
+				}
+			}
+			System.out.println(num);
+		}
 	}
 }
