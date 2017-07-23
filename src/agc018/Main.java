aa150d2f2b5f646a4b08c57cf0653cc3b2b702b4
schwarzahl <schwarzahl131072@gmail.com>
2017-07-23 21:32:08 +0900

    AGC018 30分で0完にて撤退 頭が働かない

--- src/agc018/Main.java
+++ src/agc018/Main.java
@@ -1,6 +1,8 @@
 package agc018;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 	public static void main(String[] args){
@@ -11,6 +13,20 @@ public class Main {
 	private void solveA() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		int K = sc.nextInt();
+		Set<Integer> set = new HashSet<>();
+		Set<Integer> modSet = new HashSet<>();
+		for (int i = 0; i < N; i++) {
+			int tmp = sc.nextInt();
+			set.add(tmp);
+			if (tmp >= K) {
+				modSet.add(tmp % K);
+			}
+		}
+		while () {
+
+		}
+		int[] A = new int[1000000001];
 		System.out.println(N);
 	}
 }
