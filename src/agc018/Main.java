de64099c9276b47841e014ead7a248d0ba0d9bb8
schwarzahl <schwarzahl131072@gmail.com>
2017-07-23 20:56:45 +0900

    AGC018 first commit.

---
+++ src/agc018/Main.java
@@ -0,0 +1,16 @@
+package agc018;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Main main = new Main();
+		main.solveA();
+	}
+
+	private void solveA() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+}
