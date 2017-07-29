9c809dba79ed5b1f2bc776fae175d73e0c106561
schwarzahl <schwarzahl131072@gmail.com>
2017-07-29 20:59:28 +0900

    arc079 first commit.

---
+++ src/arc079/Main.java
@@ -0,0 +1,20 @@
+package arc079;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Main main = new Main();
+		main.solveC();
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(N);
+	}
+}
