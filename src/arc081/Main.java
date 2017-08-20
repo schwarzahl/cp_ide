3f60a737e6942d5177d57414258e67017a5b7bc7
schwarzahl <schwarzahl131072@gmail.com>
2017-08-20 21:11:38 +0900

    ARC081 C問題 MLE 配列1億個はダメだった

--- src/arc081/Main.java
+++ src/arc081/Main.java
@@ -12,7 +12,27 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
-		System.out.println(N * 3 + 2);
+		int[] a = new int[100000001];
+		for (int i = 0; i < N; i++) {
+			a[sc.nextInt()]++;
+		}
+		long max = 0;
+		long secMax = 0;
+		for (int i = 100000000; i >= 0 && secMax == 0; i--) {
+			if (max == 0) {
+				if (a[i] > 3) {
+					max = i;
+					secMax = i;
+				} else if (a[i] > 1) {
+					max = i;
+				}
+			} else {
+				if (a[i] > 1) {
+					secMax = i;
+				}
+			}
+		}
+		System.out.println(max * secMax);
 	}
 
 	private void solveD() {
