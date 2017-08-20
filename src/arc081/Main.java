53c2f6563a1ae885a9411996874a00a2ea7bc58d
schwarzahl <schwarzahl131072@gmail.com>
2017-08-20 20:45:35 +0900

    arc081 first commit

---
+++ src/arc081/Main.java
@@ -0,0 +1,31 @@
+package arc081;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.solveC();
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		System.out.println(N * 3 + 2);
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		System.out.println(N);
+	}
+
+	private void solveE() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		System.out.println(N);
+	}
+}
