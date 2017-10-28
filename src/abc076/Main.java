cc789d004b0a388dd4fa8888c1fea5a24ec8a8a3
schwarzahl <schwarzahl131072@gmail.com>
2017-10-28 21:50:02 +0900

    ABC076 C問題AC

--- src/abc076/Main.java
+++ src/abc076/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveB();
+		main.solveC();
 	}
 
 	private void solveA() {
@@ -28,5 +28,37 @@ public class Main {
 			}
 		}
 		System.out.println(output);
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		String T = sc.next();
+		int SL = S.length();
+		int TL = T.length();
+		for (int s_ind = SL - TL; s_ind >= 0; s_ind--) {
+			int t_ind = 0;
+			for (; t_ind < TL; t_ind++) {
+				if (S.charAt(s_ind + t_ind) != '?' && S.charAt(s_ind + t_ind) != T.charAt(t_ind)) {
+					break;
+				}
+			}
+			if (t_ind == TL) {
+				for (int o_ind = 0; o_ind < SL; o_ind++) {
+					if (s_ind <= o_ind && o_ind < s_ind + TL) {
+						System.out.print(T.charAt(o_ind - s_ind));
+					} else {
+						if (S.charAt(o_ind) == '?') {
+							System.out.print('a');
+						} else {
+							System.out.print(S.charAt(o_ind));
+						}
+					}
+				}
+				System.out.println();
+				return;
+			}
+		}
+		System.out.println("UNRESTORABLE");
 	}
 }
