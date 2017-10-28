ab6c954d6a8b44fa14fb01a7f05e96ff9200b56b
schwarzahl <schwarzahl131072@gmail.com>
2017-10-28 21:37:58 +0900

    ABC076 B問題AC

--- src/abc076/Main.java
+++ src/abc076/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveA();
+		main.solveB();
 	}
 
 	private void solveA() {
@@ -13,5 +13,20 @@ public class Main {
 		int R = sc.nextInt();
 		int G = sc.nextInt();
 		System.out.println(2 * G - R);
+	}
+
+	private void solveB() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int output = 1;
+		for (int i = 0; i < N; i++) {
+			if (output < K) {
+				output *= 2;
+			} else {
+				output += K;
+			}
+		}
+		System.out.println(output);
 	}
 }
