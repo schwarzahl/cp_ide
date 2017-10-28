75080f6821f6f0159c6c75b4f05507321ca70c38
schwarzahl <schwarzahl131072@gmail.com>
2017-10-28 21:30:46 +0900

    ABC076 start.

---
+++ src/abc076/Main.java
@@ -0,0 +1,15 @@
+package abc076;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Main main = new Main();
+		main.solveA();
+	}
+
+	private void solveA() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() * 5);
+	}
+}
