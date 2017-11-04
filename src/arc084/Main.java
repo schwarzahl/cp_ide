f43e2e5fcc8ec49a46c792bc053361cb703c5f4a
schwarzahl <schwarzahl131072@gmail.com>
2017-11-04 22:04:32 +0900

    ARC084 D問題 WAとTLE やり方が雑すぎた

--- src/arc084/Main.java
+++ src/arc084/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Main main = new Main();
-		main.solveC();
+		main.solveD();
 	}
 
 	private void solveC() {
@@ -41,5 +41,35 @@ public class Main {
 			sum+=((long)i_a)*((long)N-(long)i_c);
 		}
 		System.out.println(sum);
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		long K = sc.nextLong();
+		int min = sum(K);
+		for (long l = 1; l < pow(min); l++) {
+			int tmp = sum(l*K);
+			if (min > tmp) {
+				min = tmp;
+			}
+		}
+		System.out.println(min);
+	}
+
+	private long pow(int digit) {
+		long ret = 1;
+		for (int i = 0; i < digit; i++) {
+			ret *= 10;
+		}
+		return ret;
+	}
+
+	private int sum(long num) {
+		int ret = 0;
+		while (num > 0) {
+			ret += num % 10;
+			num /= 10;
+		}
+		return ret;
 	}
 }
