33996b0d1e69297d98e843d4ad0500e081aa6e5d
schwarzahl <schwarzahl131072@gmail.com>
2017-11-04 21:27:39 +0900

    ARC084 C問題 WA 何か考慮が足りていない

--- src/arc084/Main.java
+++ src/arc084/Main.java
@@ -1,9 +1,7 @@
 package arc084;
 
-import java.util.Collections;
-import java.util.Map.Entry;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class Main {
 	public static void main(String[] args) {
@@ -14,7 +12,34 @@ public class Main {
 	private void solveC() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-
-		System.out.println(N*N*N);
+		int[] aList = new int[N];
+		int[] bList = new int[N];
+		int[] cList = new int[N];
+		for (int i = 0; i < N; i++) {
+			aList[i] = sc.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			bList[i] = sc.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			cList[i] = sc.nextInt();
+		}
+		Arrays.sort(aList);
+		Arrays.sort(bList);
+		Arrays.sort(cList);
+		int i_a = 0;
+		int i_b = 0;
+		int i_c = 0;
+		long sum = 0L;
+		for (; i_b < N; i_b++) {
+			while (i_a < N && aList[i_a] < bList[i_b]) {
+				i_a++;
+			}
+			while (i_c < N && bList[i_b] >= cList[i_c]) {
+				i_c++;
+			}
+			sum+=(i_a)*(N-i_c);
+		}
+		System.out.println(sum);
 	}
 }
