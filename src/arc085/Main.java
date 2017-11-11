ff3b52c4e57acaade5ef4d607bf8327d652ce35c
schwarzahl <schwarzahl131072@gmail.com>
2017-11-11 21:21:31 +0900

    ARC085 C問題AC こんなのでいいのか・・・

--- src/arc085/Main.java
+++ src/arc085/Main.java
@@ -11,6 +11,24 @@ public class Main {
 	private void solveC() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		System.out.println(2*N+5);
+		int M = sc.nextInt();
+		int time = N * 100 + M * 1800;
+		int pi = 1;
+		for (int i = 0; i < M; i++) {
+			pi *= 2;
+		}
+		double prev = -1.0;
+		double current = 0.0;
+		int k = 1;
+		while (Math.ceil(prev) != Math.ceil(current)) {
+			prev = current;
+			double tmp = 1.0 * k * time / pi;
+			for (int d = 1; d < k; d++) {
+				tmp *= (1.0 - 1.0 / pi);
+			}
+			current += tmp;
+			k++;
+		}
+		System.out.println(100*Math.round(Math.ceil(current/100)));
 	}
 }
