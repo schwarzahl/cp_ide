f253612a10067848e92b024c1032f9df01fb4c59
schwarzahl <schwarzahl131072@gmail.com>
2017-11-11 21:56:04 +0900

    ARC085 E問題 愚直に解いたけどWAだった あれー・・・

--- src/arc085/Main.java
+++ src/arc085/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Main main = new Main();
-		main.solveD();
+		main.solveE();
 	}
 
 	private void solveC() {
@@ -47,5 +47,42 @@ public class Main {
 			ans2 = Math.abs(a[N - 1] - a[N - 2]);
 		}
 		System.out.println(ans1 > ans2 ? ans1 : ans2);
+	}
+
+	private void solveE() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] a = new int[N + 1];
+		long sum = 0L;
+		for (int i = 1; i <= N; i++) {
+			a[i] = sc.nextInt();
+			sum += a[i];
+		}
+		int min_x = -1;
+		while (min_x != 0) {
+			min_x = 0;
+			long min = 0L;
+			for (int x = 1; x <= 100; x++) {
+				long tmp = 0L;
+				for (int i = 1; i <= N; i++) {
+					if (i % x == 0) {
+						tmp += a[i];
+					}
+				}
+				if (min > tmp) {
+					min = tmp;
+					min_x = x;
+				}
+			}
+			if (min_x != 0) {
+				for (int i = 1; i <= N; i++) {
+					if (i % min_x == 0) {
+						a[i] = 0;
+					}
+				}
+				sum -= min;
+			}
+		}
+		System.out.println(sum);
 	}
 }
