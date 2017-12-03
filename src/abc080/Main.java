1a3ccb7d8af65d173de29da4150ac6d0326e9f21
schwarzahl <schwarzahl131072@gmail.com>
2017-12-03 21:19:55 +0900

    ABC080 A問題 B問題はAC D問題もAC

--- src/abc080/Main.java
+++ src/abc080/Main.java
@@ -5,12 +5,61 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Main main = new Main();
-		main.solveA();
+		main.solveD();
 	}
 
 	private void solveA() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		System.out.println(N * 2 + 5);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(N * A < B ? N * A : B);
+	}
+
+	private void solveB() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int tmp = N;
+		int sum = 0;
+		while (tmp > 0) {
+			sum += tmp % 10;
+			tmp /= 10;
+		}
+		if (N % sum == 0) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int C = sc.nextInt();
+		int[][] M = new int[C + 1][];
+		for (int j = 1; j <= C; j++) {
+			M[j] = new int[100001];
+		}
+		for (int i = 1; i <= N; i++) {
+			int s = sc.nextInt();
+			int t = sc.nextInt();
+			int c = sc.nextInt();
+			for (int j = s; j <= t; j++) {
+				M[c][j] = 1;
+			}
+		}
+		int max = 0;
+		for (int i = 1; i <= 100000; i++) {
+			int sum = 0;
+			for (int j = 1; j <= C; j++) {
+				if (M[j][i] == 1) {
+					sum++;
+				}
+			}
+			if (max < sum) {
+				max = sum;
+			}
+		}
+		System.out.println(max);
 	}
 }
