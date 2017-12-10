ef2cf31175cbbedd7c62916d83a4f3fe52e53c48
schwarzahl <schwarzahl131072@gmail.com>
2017-12-10 21:08:02 +0900

    ARC086 C問題 一発AC

--- src/arc086/Main.java
+++ src/arc086/Main.java
@@ -1,5 +1,8 @@
 package arc086;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,6 +14,24 @@ public class Main {
 	private void solveC() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		System.out.println(N * N + 5);
+		int K = sc.nextInt();
+		int A[] = new int[200001];
+
+		for (int i = 0; i < N; i++) {
+			A[sc.nextInt()]++;
+		}
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i <= 200000; i++) {
+			if (A[i] > 0) {
+				list.add(A[i]);
+			}
+		}
+		Collections.sort(list);
+		int erase = list.size() - K;
+		if (erase > 0) {
+			System.out.println(list.subList(0, erase).stream().mapToInt(a -> a).sum());
+		} else {
+			System.out.println(0);
+		}
 	}
 }
