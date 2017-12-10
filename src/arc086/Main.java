c8aa231bb29a57bf6169523b8adb847627928c92
schwarzahl <schwarzahl131072@gmail.com>
2017-12-10 21:28:25 +0900

    ARC086 D問題 一発AC

--- src/arc086/Main.java
+++ src/arc086/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Main main = new Main();
-		main.solveC();
+		main.solveD();
 	}
 
 	private void solveC() {
@@ -30,6 +30,52 @@ public class Main {
 		int erase = list.size() - K;
 		if (erase > 0) {
 			System.out.println(list.subList(0, erase).stream().mapToInt(a -> a).sum());
+		} else {
+			System.out.println(0);
+		}
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int a[] = new int[N+1];
+		int max = 0;
+		int max_id = -1;
+		int min = 0;
+		int min_id = -1;
+		for (int i = 1; i <= N; i++) {
+			a[i] = sc.nextInt();
+			if (max < a[i]) {
+				max = a[i];
+				max_id = i;
+			}
+			if (min > a[i]) {
+				min = a[i];
+				min_id = i;
+			}
+		}
+		if (max >= -min && max_id != -1) {
+			System.out.println(2 * N - 2);
+			for (int i = 1; i <= N; i++) {
+				if (i == max_id) {
+					continue;
+				}
+				System.out.println(max_id + " " + i);
+			}
+			for (int i = 1; i <= N - 1; i++) {
+				System.out.println(i + " " + (i+1));
+			}
+		} else if (max <= -min && min_id != -1) {
+			System.out.println(2 * N - 2);
+			for (int i = 1; i <= N; i++) {
+				if (i == min_id) {
+					continue;
+				}
+				System.out.println(min_id + " " + i);
+			}
+			for (int i = N; i > 1; i--) {
+				System.out.println(i + " " + (i-1));
+			}
 		} else {
 			System.out.println(0);
 		}
