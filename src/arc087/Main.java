fafe5124001fd5cf890e336425e05318742373cc
schwarzahl <schwarzahl131072@gmail.com>
2017-12-16 20:59:55 +0900

    ARC087 first commit.

---
+++ src/arc087/Main.java
@@ -0,0 +1,16 @@
+package arc087;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.solveC();
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N*3);
+	}
+}
