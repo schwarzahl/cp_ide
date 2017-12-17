ed3149c099787a566749841a7472c287c1f0c4a1
schwarzahl <schwarzahl131072@gmail.com>
2017-12-17 12:50:23 +0900

    毎回問題用にメソッド作る手間がもったいないのでテンプレを作った

---
+++ src/atcoder_template/Main.java
@@ -0,0 +1,46 @@
+package atcoder_template;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.solveC();
+	}
+
+	private void solveA() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveB() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveE() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveF() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+}
