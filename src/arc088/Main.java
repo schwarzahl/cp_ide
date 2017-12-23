02ee604b858dc1e54d207c094427edd6ea2b8efa
schwarzahl <schwarzahl131072@gmail.com>
2017-12-23 20:12:13 +0900

    ARC088 first commit.

---
+++ src/arc088/Main.java
@@ -0,0 +1,147 @@
+package arc088;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.solveC();
+	}
+
+	private void solveA() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveB() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveC() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveD() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveE() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	private void solveF() {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N);
+	}
+
+	interface Graph {
+		void link(int from, int to, int cost);
+		Optional<Integer> getCost(int from, int to);
+		int getVertexNum();
+	}
+
+	interface FlowResolver {
+		int maxFlow(int from, int to);
+	}
+
+	/**
+	 * グラフの行列による実装
+	 * 接点数の大きいグラフで使うとMLEで死にそう
+	 */
+	class ArrayGraph implements Graph {
+		private Integer[][] costArray;
+		private int vertexNum;
+
+		public ArrayGraph(int n) {
+			costArray = new Integer[n][];
+			for (int i = 0; i < n; i++) {
+				costArray[i] = new Integer[n];
+			}
+			vertexNum = n;
+		}
+
+		@Override
+		public void link(int from, int to, int cost) {
+			costArray[from][to] = new Integer(cost);
+		}
+
+		@Override
+		public Optional<Integer> getCost(int from, int to) {
+			return Optional.ofNullable(costArray[from][to]);
+		}
+
+		@Override
+		public int getVertexNum() {
+			return vertexNum;
+		}
+	}
+
+	/**
+	 * DFS(深さ優先探索)による実装
+	 * 計算量はO(E*MaxFlow)のはず (E:辺の数, MaxFlow:最大フロー)
+	 */
+	class DfsFlowResolver implements FlowResolver {
+		private Graph graph;
+		public DfsFlowResolver(Graph graph) {
+			this.graph = graph;
+		}
+
+		/**
+		 * 最大フロー(最小カット)を求める
+		 * @param from 始点(source)のID
+		 * @param to 終点(target)のID
+		 * @return 最大フロー(最小カット)
+		 */
+		public int maxFlow(int from, int to) {
+			int sum = 0;
+			int currentFlow;
+			do {
+				currentFlow = flow(from, to,Integer.MAX_VALUE / 3, new boolean[graph.getVertexNum()]);
+				sum += currentFlow;
+			} while (currentFlow > 0);
+			return sum;
+		}
+
+		/**
+		 * フローの実行 グラフの更新も行う
+		 * @param from 現在いる節点のID
+		 * @param to 終点(target)のID
+		 * @param current_flow ここまでの流量
+		 * @param passed 既に通った節点か否かを格納した配列
+		 * @return 終点(target)に流した流量/戻りのグラフの流量
+		 */
+		private int flow(int from, int to, int current_flow, boolean[] passed) {
+			passed[from] = true;
+			if (from == to) {
+				return current_flow;
+			}
+			for (int id = 0; id < graph.getVertexNum(); id++) {
+				if (passed[id]) {
+					continue;
+				}
+				Optional<Integer> cost = graph.getCost(from, id);
+				if (cost.orElse(0) > 0) {
+					int nextFlow = current_flow < cost.get() ? current_flow : cost.get();
+					int returnFlow = flow(id, to, nextFlow, passed);
+					if (returnFlow > 0) {
+						graph.link(from, id, cost.get() - returnFlow);
+						graph.link(id, from, graph.getCost(id, from).orElse(0) + returnFlow);
+						return returnFlow;
+					}
+				}
+			}
+			return 0;
+		}
+	}
+}
