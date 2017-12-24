f8a8d8c65ff53bab86ef92ff598826e3830bdced
schwarzahl <schwarzahl131072@gmail.com>
2017-12-24 17:09:56 +0900

    A問題 5曲目まで耳で聞いたけど10分かかってるので後に回す

--- src/xmas_contest_2017/Main.java
+++ src/xmas_contest_2017/Main.java
@@ -6,13 +6,11 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Main main = new Main();
-		main.solveC();
+		main.solveA();
 	}
 
 	private void solveA() {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		System.out.println(N);
+		System.out.println("BBABA");
 	}
 
 	private void solveB() {
