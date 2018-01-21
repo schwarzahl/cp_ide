564852d72a87215e137b0d0201fad1b69259b46e
schwarzahl <schwarzahl131072@gmail.com>
2018-01-21 16:08:53 +0900

    テンプレートを1問のみに変更 今後はコンテスト前に問題数だけ対象パッケージ配下にコピーする運用にする

--- src/atcoder_template/Main.java
+++ src/atcoder_template/Main.java
@@ -13,43 +13,14 @@ import java.util.stream.IntStream;
 public class Main {
 	public static void main(String[] args) {
 		Main main = new Main();
-		main.solveC();
+		main.solve();
 	}
 
-	private void solveA() {
+	private void solve() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		System.out.println(N);
-	}
-
-	private void solveB() {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		System.out.println(N);
-	}
-
-	private void solveC() {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		System.out.println(N);
-	}
-
-	private void solveD() {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		System.out.println(N);
-	}
-
-	private void solveE() {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		System.out.println(N);
-	}
-
-	private void solveF() {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		System.out.println(N);
+		System.err.println(Main.class.getPackage().getName());
 	}
 
 	interface Graph {
