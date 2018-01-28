fe42cbc08e84129150c2814da21aa0a9cff54d8e
schwarzahl <schwarzahl131072@gmail.com>
2018-01-28 21:05:06 +0900

    arc090 C問題 AC

--- src/arc090/problemC/Main.java
+++ src/arc090/problemC/Main.java
@@ -19,8 +19,28 @@ public class Main {
 	private void solve() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		System.out.println(N);
-		System.err.println(Main.class.getPackage().getName());
+		int[] A = new int[N];
+		int[] B = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			B[i] = sc.nextInt();
+		}
+		int max = 0;
+		for (int k = 0; k < N; k++) {
+			int sum = 0;
+			for (int i = 0; i <= k; i++) {
+				sum += A[i];
+			}
+			for (int i = k; i < N; i++) {
+				sum += B[i];
+			}
+			if (max < sum) {
+				max = sum;
+			}
+		}
+		System.out.println(max);
 	}
 
 	interface CombCalculator {
