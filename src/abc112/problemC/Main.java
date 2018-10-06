6a9936fdff8f03938db06dcb08c448b54c9d810f
schwarzahl <schwarzahl131072@gmail.com>
2018-10-06 21:17:13 +0900

    ABC112 C問題 1WA なんか高さ0しかない意地悪なケースを拾えてない気がする

--- src/abc112/problemC/Main.java
+++ src/abc112/problemC/Main.java
@@ -19,8 +19,47 @@ public class Main {
 	private void solve() {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		System.out.println(N);
-		System.err.println(Main.class.getPackage().getName());
+		int[] x = new int[N];
+		int[] y = new int[N];
+		long[] h = new long[N];
+		for (int i = 0; i < N; i++) {
+			x[i] = sc.nextInt();
+			y[i] = sc.nextInt();
+			h[i] = sc.nextLong();
+		}
+		for (int cx = 0; cx <= 100; cx++) {
+			for (int cy = 0; cy <= 100; cy++) {
+				Long H = null;
+				long border = Long.MAX_VALUE / 3;
+				for (int i = 0; i < N; i++) {
+					int dx = Math.abs(x[i] - cx);
+					int dy = Math.abs(y[i] - cy);
+					if (h[i] > 0) {
+						long tmp_h = h[i] + dx + dy;
+						if (border < tmp_h) {
+							H = null;
+							break;
+						}
+						if (H == null) {
+							H = tmp_h;
+						} else {
+							if (H != tmp_h) {
+								H = null;
+								break;
+							}
+						}
+					} else {
+						if (border > dx + dy) {
+							border = dx + dy;
+						}
+					}
+				}
+				if (H != null) {
+					System.out.println(cx + " " + cy + " " + H);
+					return;
+				}
+			}
+		}
 	}
 
 	interface CombCalculator {
